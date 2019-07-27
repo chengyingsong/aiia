9561776cf440fe63715488f2cddeee92818e0c00
Eason <43727773+chengyingsong@users.noreply.github.com>
2019-07-27 16:11:27 +0800

    Add files via upload

---
+++ lr_utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+import h5py
+    
+    
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    #打开图像文件
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+  #保存字节字符串 ， non_cat or cat
+
+    #修改矩阵规模，shape[0]代表行，即把列向量变为行向量。
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
